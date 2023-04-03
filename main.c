8670c256f47a392fd8b25160639564432a81b0e4
sanix-darker <s4nixd@gmail.com>
2023-04-03 13:41:16 +0200

    feat: so farrr sooo ggoodd

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "structs.h"
+
+int main(int argc, char* argv[]) {
+    u_int8_t call = 0;
+
+    // read yaml layout
+    // get markdown components
+    // build an html tree with it
+    return EXIT_SUCCESS;
+}
